f473fd7fab710939f07e2c59cae6c3bf9df9126f
Nestor Venegas <nestor.venegas95@gmail.com>
2021-01-31 03:27:42 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,14 @@
 # Write your solution here
 #If the current second is an even number, output Even!
 #if the current second is an odd number, output odd!
-#to determine the exact time, we can rely on ruby's built in time class. in IRB, running time.now will give you the current time.
+#running time.now will give you the current time.
+
+current_time = Time.now
+current_time =
+current_time = to.i 
+
+if TRUE
+  puts "Even!"
+else
+  puts "Odd!"
+end
